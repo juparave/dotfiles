9202b8a629a0c329026c0b41d28a86e86a9849e5
Juparave <juparave@deuxbits.com>
2022-12-04 13:40:35 -0600

    Adding documentation

--- .config/nvim_lua/lua/core/keymaps.lua
+++ .config/nvim_lua/lua/core/keymaps.lua
@@ -8,7 +8,7 @@ vim.opt.laststatus = 2
 --vim.opt.autowrite = true 
 vim.opt.hidden = true
 vim.opt.cursorline = true
-vim.opt.autoread = true
+vim.opt.autoread = true -- keep block cursor on insert mode
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 2
@@ -16,16 +16,25 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+vim.opt.smarttab = true
 
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- go to previous/next line with h,l,left arrow and right arrow when cursor reaches end/beginning of line
+vim.opt.whichwrap:append "<>[]hl"
+
 -- Undo
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--if vim.fn.isdirectory(vim.inspect(vim.opt.undodir:get())) == 0 then
+--  vim.fn.mkdir(vim.inspect(vim.opt.undodir:get()), "p")
+--end
+
+vim.opt.clipboard = "unnamedplus" -- Access system clipboard
 
 no_buffers_menu = 1
 
@@ -37,9 +46,25 @@ vim.opt.fileencodings= 'utf-8'
 -- Searching
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.opt.ignorecase = false
+vim.opt.smartcase = false
 vim.opt.hlsearch = false          -- remove highlight after search
+
+-- Better search
+vim.opt.path:remove "/usr/include"
+vim.opt.path:append "**"
+-- vim.cmd [[set path=.,,,$PWD/**]] -- Set the path directly
+
+vim.opt.wildignorecase = true
+vim.opt.wildignore:append "**/node_modules/*"
+vim.opt.wildignore:append "**/.git/*"
+
+-- Treesitter based folding
+vim.cmd [[
+  set foldlevel=20
+  set foldmethod=expr
+  set foldexpr=nvim_treesitter#foldexpr()
+]]
 
 --
 -- Visual Settings
@@ -61,7 +86,7 @@ vim.opt.mouse = 'a'
 vim.opt.mousemodel = 'popup'
 
 -- Cursor
---vim.opt.
+vim.opt.guicursor = '' -- keep block cursor on insert mode
 
 --- My remaps
 -- Auto closing brackets
@@ -70,16 +95,16 @@ vim.keymap.set('i', '(<CR>', '(<CR>)<Esc>ko')
 vim.keymap.set('i', '[<CR>', '[<CR>]<Esc>ko')
 
 -- Yank to the end of line
-vim.keymap.set('n', 'Y', 'y$')
+vim.keymap.set('n', 'Y', 'y$') -- Copy from cursor to end of line
 
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>') -- clear highlights on search results
 
 -- Switching windows
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-h>', '<C-w>h') -- left
+vim.keymap.set('n', '<C-l>', '<C-w>l') -- right
+vim.keymap.set('n', '<C-k>', '<C-w>k') -- up
+vim.keymap.set('n', '<C-j>', '<C-w>j') -- down
 
 -- Buffers
 vim.keymap.set('n', '<leader>z', ':bp<CR>') -- navigate previous buffer
@@ -89,14 +114,14 @@ vim.keymap.set('n', '<leader>w', ':bn<CR>') -- navigate next buffer
 vim.keymap.set('n', '<leader>c', ':bd<CR>') -- close buffer
 
 -- Split
-vim.keymap.set('n', '<leader>h', ':<C-u>split<CR>')
-vim.keymap.set('n', '<leader>v', ':<C-u>vsplit<CR>')
+vim.keymap.set('n', '<leader>h', ':<C-u>split<CR>') -- horizontal split
+vim.keymap.set('n', '<leader>v', ':<C-u>vsplit<CR>') -- vertical split
 
 -- Move visual block
 --- HELP !!! not working, added in `init.vim`
 -- vim.keymap.set('n', 'J', ":m '>+1<CR>gv=gv")
 -- vim.keymap.set('n', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv') -- in visual mode, move block to the right
+vim.keymap.set('v', '>', '>gv') -- in visual mode, move block to the left
 
 -- print(vim.inspect(vim.opt.formatoptions:get()))
--- .config/nvim_lua/lua/core/session.lua
+++ .config/nvim_lua/lua/core/session.lua
@@ -1,6 +1,10 @@
 -- Session management
-vim.g.session_directory = "~/.vim/session"
 vim.g.session_autoload = "no"
 vim.g.session_autosave = "no"
 vim.g.session_command_aliases = 1
 
+vim.g.session_directory = "~/.vim/session"
+
+--if vim.fn.isdirectory(vim.g.session_directory) == 0 then
+--  vim.fn.mkdir(vim.g.session_directory, "p")
+--end
