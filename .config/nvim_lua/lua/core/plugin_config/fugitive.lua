a6b7cf25a2f7ba7aa3cd271eace0ae31edc4063c
Juparave <juparave@deuxbits.com>
2022-12-04 12:31:21 -0600

    Adding fugitive

---
+++ .config/nvim_lua/lua/core/plugin_config/fugitive.lua
@@ -0,0 +1,9 @@
+-- Fugitive/Git
+vim.keymap.set('n', '<Leader>ga', ':Gwrite<CR>')
+vim.keymap.set('n', '<Leader>gc', ':Git commit --verbose<CR>')
+vim.keymap.set('n', '<Leader>gsh', ':Git push<CR>')
+vim.keymap.set('n', '<Leader>gll', ':Git pull<CR>')
+vim.keymap.set('n', '<Leader>gs', ':Git<CR>')
+vim.keymap.set('n', '<Leader>gb', ':Git blame<CR>')
+vim.keymap.set('n', '<Leader>gd', ':Gvdiffsplit<CR>')
+vim.keymap.set('n', '<Leader>gr', ':GRemove<CR>')
