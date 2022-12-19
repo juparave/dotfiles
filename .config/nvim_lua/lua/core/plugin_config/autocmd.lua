311b0c7824dfcd4fc0aca2a189eabf2910fa45b1
Juan Pablo TankerMini <juparave@deuxbits.com>
2022-12-18 20:44:41 -0600

    Adding restore_cursor to autocmd

--- .config/nvim_lua/lua/core/plugin_config/autocmd.lua
+++ .config/nvim_lua/lua/core/plugin_config/autocmd.lua
@@ -17,60 +17,63 @@ function nvim_create_augroups(definitions)
 end
 
 nvim_create_augroups({
-    go_save = {
-      {"BufWritePre", "*.go", "lua vim.lsp.buf.format({ async = true, offset_encoding = 'utf-16' })"},
+  go_save = {
+    {"BufWritePre", "*.go", "lua vim.lsp.buf.format({ async = true, offset_encoding = 'utf-16' })"},
 
-      -- {"BufWritePre", "*.go", "lua go_org_imports"},
-      {"BufWritePre", "*.go", "lua goimports(500)"},
-    },
-    go_shiftwith = {
-      {"BufNewFile,BufRead", "*.go", "setlocal noexpandtab tabstop=4 shiftwidth=4 softtabstop=4"},
-    }
-  })
+    -- {"BufWritePre", "*.go", "lua go_org_imports"},
+    {"BufWritePre", "*.go", "lua goimports(500)"},
+  },
+  go_shiftwith = {
+    {"BufNewFile,BufRead", "*.go", "setlocal noexpandtab tabstop=4 shiftwidth=4 softtabstop=4"},
+  },
+  restore_cursor = {
+    { 'BufRead', '*', [[call setpos(".", getpos("'\""))]] };
+  },
+})
 
 function go_org_imports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
       end
     end
   end
+end
 
 -- ref: https://www.getman.io/posts/programming-go-in-neovim/
 function goimports(timeoutms)
-    local context = { source = { organizeImports = true } }
-    vim.validate { context = { context, "t", true } }
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
 
-    local params = vim.lsp.util.make_range_params()
-    params.context = context
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
 
-    -- See the implementation of the textDocument/codeAction callback
-    -- (lua/vim/lsp/handler.lua) for how to do this properly.
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-    if not result or next(result) == nil then return end
-    local actions = result[1].result
-    if not actions then return end
-    local action = actions[1]
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/handler.lua) for how to do this properly.
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result or next(result) == nil then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
 
-    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-    -- is a CodeAction, it can have either an edit, a command or both. Edits
-    -- should be executed first.
-    if action.edit or type(action.command) == "table" then
-      if action.edit then
-        -- local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-        local enc = "utf-16"
-        vim.lsp.util.apply_workspace_edit(action.edit, enc)
-      end
-      if type(action.command) == "table" then
-        vim.lsp.buf.execute_command(action.command)
-      end
-    else
-      vim.lsp.buf.execute_command(action)
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      -- local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+      local enc = "utf-16"
+      vim.lsp.util.apply_workspace_edit(action.edit, enc)
     end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
   end
+end
