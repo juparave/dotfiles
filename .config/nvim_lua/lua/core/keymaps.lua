f8ae8c54bad9dad71c520925876009deceb0ed41
Juan Pablo TankerMini <juparave@deuxbits.com>
2022-12-20 17:14:46 -0600

    Adding utils map, and refactoring keymaps

--- .config/nvim_lua/lua/core/keymaps.lua
+++ .config/nvim_lua/lua/core/keymaps.lua
@@ -1,46 +1,49 @@
+-- Import & assign the map() function from the utils module
+local map = require("utils").map
+
 --- My remaps
 -- Auto closing brackets
-vim.keymap.set('i', '{<CR>', '{<CR>}<Esc>ko')
-vim.keymap.set('i', '(<CR>', '(<CR>)<Esc>ko')
-vim.keymap.set('i', '[<CR>', '[<CR>]<Esc>ko')
+map('i', '{<CR>', '{<CR>}<Esc>ko')
+map('i', '(<CR>', '(<CR>)<Esc>ko')
+map('i', '[<CR>', '[<CR>]<Esc>ko')
 
 -- Special Yanks
-vim.keymap.set('n', 'Y', 'y$') -- Copy from cursor to end of line
-vim.keymap.set('n', 'x', '"_x') -- Do not yank with x
+map('n', 'Y', 'y$') -- Copy from cursor to end of line
+map('n', 'x', '"_x') -- Do not yank with x
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>') -- clear highlights on search results
+map('n', '<leader>h', ':nohlsearch<CR>') -- clear highlights on search results
 
 -- Switching windows
-vim.keymap.set('n', '<C-h>', '<C-w>h') -- left
-vim.keymap.set('n', '<C-l>', '<C-w>l') -- right
-vim.keymap.set('n', '<C-k>', '<C-w>k') -- up
-vim.keymap.set('n', '<C-j>', '<C-w>j') -- down
+map('n', '<C-h>', '<C-w>h') -- left
+map('n', '<C-l>', '<C-w>l') -- right
+map('n', '<C-k>', '<C-w>k') -- up
+map('n', '<C-j>', '<C-w>j') -- down
 
 -- Buffers
-vim.keymap.set('n', '<leader>z', ':bp<CR>') -- navigate previous buffer
-vim.keymap.set('n', '<leader>q', ':bp<CR>') -- navigate previous buffer
-vim.keymap.set('n', '<leader>x', ':bn<CR>') -- navigate next buffer
-vim.keymap.set('n', '<leader>w', ':bn<CR>') -- navigate next buffer
-vim.keymap.set('n', '<leader>c', ':bd<CR>') -- close buffer
+map('n', '<leader>z', ':bp<CR>') -- navigate previous buffer
+map('n', '<leader>q', ':bp<CR>') -- navigate previous buffer
+map('n', '<leader>x', ':bn<CR>') -- navigate next buffer
+map('n', '<leader>w', ':bn<CR>') -- navigate next buffer
+map('n', '<leader>c', ':bd<CR>') -- close buffer
 
 -- Split
-vim.keymap.set('n', '<leader>h', ':<C-u>split<CR>') -- horizontal split
-vim.keymap.set('n', '<leader>v', ':<C-u>vsplit<CR>') -- vertical split
+map('n', '<leader>h', ':<C-u>split<CR>') -- horizontal split
+map('n', '<leader>v', ':<C-u>vsplit<CR>') -- vertical split
 
 -- Move visual block
 vim.cmd [[vnoremap J :m '>+1<CR>gv=gv]]
 vim.cmd [[vnoremap K :m '<-2<CR>gv=gv]]
 -- having trouble to set it in lua
--- vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
--- vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+-- map('v', 'J', ":m '>+1<CR>gv=gv")
+-- map('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('v', '<', '<gv') -- in visual mode, move block to the right
-vim.keymap.set('v', '>', '>gv') -- in visual mode, move block to the left
+map('v', '<', '<gv') -- in visual mode, move block to the right
+map('v', '>', '>gv') -- in visual mode, move block to the left
 
 -- Undotree
-vim.keymap.set('n', '<leader>u', ':UndotreeShow<CR>')
+map('n', '<leader>u', ':UndotreeShow<CR>')
 
 -- Paste without copying replaced text
-vim.keymap.set('x', '<leader>p', "\"_dP")
+map('x', '<leader>p', "\"_dP")
 
 -- print(vim.inspect(vim.opt.formatoptions:get()))
---
+++ .config/nvim_lua/lua/utils.lua
@@ -0,0 +1,14 @@
+-- Our lua/utils.lua file
+-- ref: https://blog.devgenius.io/create-custom-keymaps-in-neovim-with-lua-d1167de0f2c2
+
+local M = {}
+
+function M.map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+return M
