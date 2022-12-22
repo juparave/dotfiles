45a0f7af19cdaec1da78e14c552ab70d3706d7a9
Juparave <juparave@deuxbits.com>
2022-12-22 12:36:41 -0600

    Formatting theme.lua file

--- .config/nvim_lua/lua/core/theme.lua
+++ .config/nvim_lua/lua/core/theme.lua
@@ -1,27 +1,27 @@
 function ColorMyPencils(color)
-	color = color or "tokyonight"
-	vim.cmd.colorscheme(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
 
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    --	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    --	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 require('nightfox').setup({
-  options = {
-    styles = {
- --     comments = "italic",
-      keywords = "bold",
-      types = "bold",
-      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "NONE",
-      constants = "NONE",
-      functions = "NONE",
-      numbers = "NONE",
-      operators = "NONE",
-      strings = "NONE",
-      variables = "NONE",
+    options = {
+        styles = {
+            --     comments = "italic",
+            keywords = "bold",
+            types = "bold",
+            comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+            conditionals = "NONE",
+            constants = "NONE",
+            functions = "NONE",
+            numbers = "NONE",
+            operators = "NONE",
+            strings = "NONE",
+            variables = "NONE",
+        }
     }
-  }
 })
 
 
@@ -45,7 +45,6 @@ function Dawnfox()
     }
 end
 
-
 function Nightfox()
     vim.cmd.colorscheme("nightfox")
     require("lualine").setup {
@@ -54,5 +53,3 @@ function Nightfox()
         },
     }
 end
-
-
