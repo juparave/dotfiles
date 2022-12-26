b340f6e5c53234ee067a584b4e20cb4494e9f988
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-12-26 22:43:52 +0000

    Improving telescope with fzf extension

--- .config/nvim_lua/lua/core/plugin_config/telescope.lua
+++ .config/nvim_lua/lua/core/plugin_config/telescope.lua
@@ -6,40 +6,49 @@ local actions = require('telescope.actions')
 
 
 telescope.setup {
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key",
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-      }
-    }
-  },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-      --   picker_config_key = value,
-      --   ...
-      -- }
-      -- Now the picker_config_key will be applied every time you call this
-      -- builtin picker
+    defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        mappings = {
+            i = {
+                -- map actions.which_key to <C-h> (default: <C-/>)
+                -- actions.which_key shows the mappings for your picker,
+                -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                ["<C-h>"] = "which_key",
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+            }
+        }
+    },
+    pickers = {
+        -- Default configuration for builtin pickers goes here:
+        -- picker_name = {
+        --   picker_config_key = value,
+        --   ...
+        -- }
+        -- Now the picker_config_key will be applied every time you call this
+        -- builtin picker
     },
     extensions = {
-      -- Your extension configuration goes here:
-      -- extension_name = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
         --   extension_config_key = value,
         -- }
         -- please take a look at the readme of the extension you want to configure
-      }
-
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        }
     }
+
+}
+
+require('telescope').load_extension('fzf')
 
 
 vim.keymap.set('n', '<leader>e', builtin.find_files, { desc = "telescope find files" })
@@ -47,4 +56,3 @@ vim.keymap.set('n', '<leader>e', builtin.find_files, { desc = "telescope find fi
 vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = "telescope live grep" })
 vim.keymap.set('n', '<leader>t', builtin.help_tags, { desc = "telescope help tags" })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = "telescope buffers" })
-
