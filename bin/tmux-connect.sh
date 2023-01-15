11d6e967f5f30786413757bdf558ed49e319bf24
Juan Pablo TankerMini <juparave@deuxbits.com>
2023-01-15 10:49:21 -0600

    tmux connect

---
+++ bin/tmux-connect.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# connect to a remote tmux session
+# ./tmux-connect.sh remote.workstation.com work
+
+# default values
+host=${1:-"remote.workstation.com"}
+session=${2:-"work"}
+
+echo "Connecting to remote tmux session $session on $host"
+
+# Redirect stderr to a variable
+error=$(ssh -t $host tmux attach -t $session 2>&1)
+
+# Check if there was an error
+if [ $? -ne 0 ]; then
+  echo "Error: $error"
+  echo "Please enable the remote tmux session first"
+  echo "eg. tmux new -s work -d"
+else
+  echo "Successfully connected to remote tmux session $session on $host"
+fi
+
