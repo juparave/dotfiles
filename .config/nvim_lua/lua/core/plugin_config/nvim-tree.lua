b81e7d518fb99dcd429cfb5564da44bd2617cb70
Juan Pablo Ramirez Vega <juparave@deuxbits.com>
2024-02-08 12:28:10 -0600

    changing nvim-tree folder icons

--- .config/nvim_lua/lua/core/plugin_config/nvim-tree.lua
+++ .config/nvim_lua/lua/core/plugin_config/nvim-tree.lua
@@ -2,30 +2,30 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require('nvim-tree').setup({
-  -- change folder arrow icons
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
+    -- change folder arrow icons
+    renderer = {
+        icons = {
+            glyphs = {
+                folder = {
+                    arrow_closed = "", -- arrow when folder is closed
+                    arrow_open = "", -- arrow when folder is open
+                },
+            },
         },
-      },
     },
-  },
-  -- disable window_picker for
-  -- explorer to work well with
-  -- window splits
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      },
+    -- disable window_picker for
+    -- explorer to work well with
+    -- window splits
+    actions = {
+        open_file = {
+            window_picker = {
+                enable = false,
+            },
+        },
     },
-  },
-  -- 	git = {
-  -- 		ignore = false,
-  -- 	},
+    -- 	git = {
+    -- 		ignore = false,
+    -- 	},
 })
 
 vim.keymap.set('n', '<F3>', ':NvimTreeFindFileToggle<CR>')
