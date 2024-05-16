dc457171df39c9c5fc877d81dac3381070cb393c
Juan Pablo Ramirez Vega <juparave@deuxbits.com>
2024-05-16 11:42:19 -0600

    removinf build cache too

--- bin/clean_docker.sh
+++ bin/clean_docker.sh
@@ -3,6 +3,10 @@
 echo "Removing all stopped containers"
 docker container prune -f
 
+# Remove build cache objects
+echo "Removing build cache objects"
+docker builder prune -f
+
 # Remove all dangling images
 echo "Removing all dangling images"
 docker image prune -a -f
