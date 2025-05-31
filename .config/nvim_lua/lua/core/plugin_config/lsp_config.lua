4fd2526857a7ade9356f22caf99e0c62629763aa
Juparave <juparave@deuxbits.com>
2025-05-30 20:08:09 -0600

    fix: lsp for new neovim

--- .config/nvim_lua/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim_lua/lua/core/plugin_config/lsp_config.lua
@@ -35,24 +35,18 @@ local on_attach = function(client, bufnr)
     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end
 
-require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        }
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-    -- ["rust_analyzer"] = function ()
-    --     require("rust-tools").setup {}
-    -- end,
-    -- ["eslint"] = function ()
-    --     vim.cmd.LspStop('eslint')
-    -- end
+local lspconfig = require("lspconfig")
+lspconfig.lua_ls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+lspconfig.pyright.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
 }
 
 
