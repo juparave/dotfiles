c085ef1898486efa95b1411a794290731b629f4e
Juan Pablo Ramirez Vega <juparave@deuxbits.com>
2025-07-09 12:36:11 -0600

    feat(lsp): improve config formatting, add svelte support, and update mason settings
    - Refactor lsp_config.lua for consistent formatting and style
    - Add Svelte LSP (svelteserver) configuration
    - Update mason-lspconfig to exclude svelteserver from auto-enable
    - Use table syntax consistently for setup calls
    - Minor improvements to ESLint and Angular LSP setup

--- .config/nvim_lua/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim_lua/lua/core/plugin_config/lsp_config.lua
@@ -1,10 +1,10 @@
-require("lsp-format").setup {}
+require("lsp-format").setup({})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    require("lsp-format").on_attach(client, args.buf)
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        require("lsp-format").on_attach(client, args.buf)
+    end,
 })
 
 -- lemminx, substitute for xmlformat
@@ -13,20 +13,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 -- LSP Format on Save
 local format_on_save = function(bufnr)
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format({ async = false })
-    end,
-  })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({ async = false })
+        end,
+    })
 end
 
 -- Define capabilities and on_attach BEFORE using them
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = function(client, bufnr)
-
     if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
+        vim.cmd.LspStop("eslint")
         return
     end
 
@@ -49,13 +48,13 @@ end
 -- Set up mason for package management only (no automatic LSP setup)
 require("mason").setup()
 
-require("mason-lspconfig").setup {
+require("mason-lspconfig").setup({
     ensure_installed = { "lua_ls", "gopls", "pyright", "angularls", "ts_ls", "eslint" }, -- Specify the servers you want to ensure are installed
-    automatic_installation = false,                                -- Disable automatic installation
+    automatic_installation = false,                                                   -- Disable automatic installation
     automatic_enable = {
-        exclude = { "gopls", "angularls" },                        -- Exclude servers from automatic enabling
-    }
-}
+        exclude = { "gopls", "angularls", "svelteserver" },                           -- Exclude servers from automatic enabling
+    },
+})
 
 -- Manual setup for each server to ensure only one instance
 local lspconfig = require("lspconfig")
@@ -65,7 +64,7 @@ local lspconfig = require("lspconfig")
 --     capabilities = capabilities,
 -- }
 
-lspconfig.gopls.setup {
+lspconfig.gopls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     cmd = { "gopls" }, -- Use simple command, let mason handle the path
@@ -78,38 +77,46 @@ lspconfig.gopls.setup {
             gofumpt = true,
         },
     },
-}
+})
 
-lspconfig.angularls.setup {
+lspconfig.angularls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     cmd = { "angularls" }, -- Use simple command, let mason handle the path
     filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
     root_dir = lspconfig.util.root_pattern("angular.json", "package.json"),
-}
+})
+
+lspconfig.svelte.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "svelteserver", "--stdio" }, -- Use simple command, let mason handle the path
+    filetypes = { "svelte" },
+    root_dir = lspconfig.util.root_pattern("svelte.config.js", "package.json"),
+})
 
 -- lspconfig.pyright.setup {
 --     on_attach = on_attach,
 --     capabilities = capabilities,
 -- }
 
-local util = require 'lspconfig.util'
-require 'lspconfig'.eslint.setup {
+local util = require("lspconfig.util")
+require("lspconfig").eslint.setup({
     -- `vscode-eslint-language-server` is a linting engine for JavaScript / Typescript.
     -- It can be installed via `npm`:
     -- npm i -g vscode-langservers-extracted
     -- Copied from nvim-lspconfig/lua/lspconfig/server_conigurations/eslint.js
     root_dir = util.root_pattern(
-        '.eslintrc',
-        '.eslintrc.js',
-        '.eslintrc.cjs',
-        '.eslintrc.yaml',
-        '.eslintrc.yml',
-        '.eslintrc.json'
+        ".eslintrc",
+        ".eslintrc.js",
+        ".eslintrc.cjs",
+        ".eslintrc.yaml",
+        ".eslintrc.yml",
+        ".eslintrc.json"
     -- Disabled to prevent "No ESLint configuration found" exceptions
     -- 'package.json',
     ),
-}
+})
 
 vim.diagnostic.config({
     virtual_text = true,
